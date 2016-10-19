fc187ced08f1fd78746d4d41505ce3953b1928af
sufinsha <sufinsha@gmail.com>
2016-10-19 15:26:00 +0530

    # change in response keys

--- app/assets/javascripts/channels/appearance.js
+++ app/assets/javascripts/channels/appearance.js
@@ -5,7 +5,7 @@ CreateAppearanceChannel = function() {
     connected: function() {},
     disconnected: function() {},
     received: function(data) {
-      if (data.appear == true){
+      if (data.is_online == true){
         var online_html = '<div class="online"></div>'
         $('#user_' + data.user_id).html(online_html);
       }
